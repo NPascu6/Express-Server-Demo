d02bff2c911e8bde80fd61832a103b342c112395
Pascu Norbert <norbert.pascu@softvision.ro>
2020-01-16 17:46:52 +0200

    ADded Jwt and hasing to sign-up sing-in methods.

--- src/routes/authentificationRoutes.js
+++ src/routes/authentificationRoutes.js
@@ -6,7 +6,6 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-
     const { email, password } = req.body;
 
     try {
@@ -20,8 +19,25 @@ router.post('/signup', async (req, res) => {
         res.status(422).send(err.message);
         console.log(err.message);
     }
+});
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).send({ error: 'Must Provide Password or email.' })
+    };
 
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).send({ error: "Email not found." });
+    }
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+        res.send({ token: token });
+    } catch (err) {
+        return res.status(422).send('Invalid password or email.');
+    }
 });
 
 module.exports = router;
